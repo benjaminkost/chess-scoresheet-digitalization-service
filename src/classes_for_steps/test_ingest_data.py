ca437f5450ad573aa20e43aeca172f978dd51240
Ben Kostka <mail@ben-kostka.de>
2025-04-30 16:38:43 +0200

    (had git problem - was the commit for merge with main) Added tests to test_ingest_data.py, changing ImageDataIngestorImpl to HuggingFaceImageIngestor and import in uat

--- src/classes_for_steps/test_ingest_data.py
+++ src/classes_for_steps/test_ingest_data.py
@@ -1,21 +1,42 @@
 import unittest
-
-from src.classes_for_steps.ingest_data import ImageDataIngestorImpl
+from src.classes_for_steps.ingest_data import HuggingFaceImageIngestor
+from PIL import Image
 
 
 class ImageDataIngestorTest(unittest.TestCase):
-
-    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_ReturnsDS(self):
+    @classmethod
+    def setUpClass(cls):
         # Give
-        owner = "Chesscorner"
-        dataset_name = "HCS_Dataset-csv"
-        ingestor = ImageDataIngestorImpl()
+        owner = "BenjaminKost"
+        dataset_name = "unprocessed_hcs"
+        ingestor = HuggingFaceImageIngestor()
 
         # When
-        dataset = ingestor.ingest_image_dataset_from_huggingface(owner=owner, dataset_name=dataset_name)
+        cls.dataset = ingestor.ingest_image_dataset_from_huggingface(owner=owner, dataset_name=dataset_name)
+
+    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_ReturnsDS(self):
+        # Then
+        self.assertEqual(206, len(self.dataset["train"]))  # add assertion here
+
+    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_and_check_datatypes_Returns_image(self):
 
         # Then
-        self.assertEqual(206, len(dataset["train"]))  # add assertion here
+        self.assertIsInstance(self.dataset["train"][0]["image"], Image.Image)
+
+    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_and_check_datatypes_Returns_str(self):
+
+        # Then
+        self.assertIsInstance(self.dataset["train"][0]["labels"], list)
+
+    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_and_check_datatypes_of_column_image_Returns_list(self):
+
+        # Then
+        self.assertIsInstance(self.dataset["train"]["image"], list)
+
+    def test_ingest_image_dataset_from_huggingface_Chesscorner_HCS_Dataset_csv_and_check_datatypes_of_column_text_Returns_list(self):
+
+        # Then
+        self.assertIsInstance(self.dataset["train"]["labels"], list)
 
 
 if __name__ == '__main__':
