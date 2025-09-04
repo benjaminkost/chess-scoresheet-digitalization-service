2b70628d592c9ad8ba2f39e178e286cf0fdb1f89
Ben Kostka <mail@ben-kostka.de>
2025-09-04 19:43:06 +0200

    feature-70-interact-with-docker-containers-as-model-for-inference: add health client method + test + add exception handling

--- src/api/clients/mlflow_model_client.py
+++ src/api/clients/mlflow_model_client.py
@@ -1,13 +1,50 @@
+import logging
 import os
+from typing import Any
 
-from PIL.Image import Image
 import requests
+from torch import Tensor
 
+# configure logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
-def predict(image: Image):
-    # Call mlflow endpoint
+def predict(pixel_values: Tensor) -> Any | None:
+    """
+    Calling the endpoint of the mlflow container /invocation to get a prediction from model
+
+    :param pixel_values: Torch.Tensor that is the tokenized image for inference.
+    :return: json (dict) for the prediction.
+    """
+    # Setup url for mlflow prediction
     mlflow_docker_controller_base_url = os.getenv("MLFLOW_DOCKER_BASE_URL")
     prediction_url = f"{mlflow_docker_controller_base_url}/invocations"
-    prediction = requests.post(prediction_url, data=image.tobytes())
 
-    return prediction.json()
+    # Call mlflow endpoint
+    try:
+        prediction = requests.post(prediction_url, data=pixel_values)
+        return prediction.json()
+    except requests.RequestException as e:
+        logger.error(f"Error when calling the /invocation endpoint of mlflow container: {e}")
+
+
+def check_health() -> bool:
+    """
+    Calling the endpoint of the mlflow container /health to get a health status from container
+
+    :return: True if health, False otherwise.
+    """
+    # Setup url for mlflow prediction
+    mlflow_docker_controller_base_url = os.getenv("MLFLOW_DOCKER_BASE_URL")
+    health_url = f"{mlflow_docker_controller_base_url}/health"
+
+    # Call mlflow endpoint
+    try:
+        health_status = requests.get(health_url).status_code
+        return health_status == 200
+    except requests.RequestException as e:
+        logger.error(f"Error when calling the /invocation endpoint of mlflow container: {e}")
+        return False
--- tests/unit/api/clients/test_mlflow_model_client.py
+++ tests/unit/api/clients/test_mlflow_model_client.py
@@ -1,17 +1,28 @@
-from pathlib import Path
-from PIL import Image
-from src.api.clients.mlflow_model_client import predict
+import torch
+from src.api.clients.mlflow_model_client import predict, check_health
 
-def test_predict(mocker, get_data_path: Path):
+def test_predict_with_torch_tensor_return_prediction(mocker):
     # Arrange
-    image = Image.open(get_data_path / "images" / "001_0.png")
+    pixel_values = torch.tensor([[0, 0, 0], [1, 1, 1]])
     api_json_response = {"prediction": "abc"}
     mock_post = mocker.patch("requests.post")
     mock_post.return_value.json.return_value = api_json_response
 
     # Act
-    res = predict(image)
+    res = predict(pixel_values)
 
     # Assert
     mock_post.assert_called_once()
     assert res == api_json_response
+
+def test_check_health_returns_ok(mocker):
+    # Arrange
+    mock_post = mocker.patch("requests.get")
+    mock_post.return_value.status_code = 200
+
+    # Act
+    res = check_health()
+
+    # Assert
+    mock_post.assert_called_once()
+    assert res == True
